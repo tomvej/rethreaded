7bd6d34859c4b117067078b055a16046eaffbe17
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-02-13 11:47:47 +0100

    redux plugged in

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 const Component = () => (
-    <div>It works!</div>
+    <Provider store={store}>
+        <div>It works!</div>
+    </Provider>
 );
 
 render(
---
+++ src/store.ts
@@ -0,0 +1,15 @@
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import reduxFreeze from 'redux-freeze';
+import reducer from './reducer';
+
+const middlewareList = [];
+
+if (__DEVELOPMENT__) {
+    middlewareList.push(reduxFreeze);
+}
+
+export default createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middlewareList)),
+);
