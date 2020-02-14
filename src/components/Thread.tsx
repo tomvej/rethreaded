8045bd00856d07e0bb04304d7806163f561c4e24
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-02-14 15:45:00 +0100

    thread component styling

--- src/components/Thread.tsx
+++ src/components/Thread.tsx
@@ -6,14 +6,15 @@ import style from './Thread.scss';
 
 type ThreadColorProps = {
     color: Color;
+    label?: string;
 };
 
-const Thread: FunctionComponent<ThreadColorProps> = ({color}) => (
+const Thread: FunctionComponent<ThreadColorProps> = ({label, color}) => (
     <div
         className={style.main}
         style={{backgroundColor: toHex(color)}}
     >
-
+        {label}
     </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
 
+import './style.scss';
+
 import {Thread} from './components';
 import {fromHex} from './utils/color';
 
 const Component: FunctionComponent = () => (
     <Provider store={store}>
         <div>
-            <Thread color={fromHex('#ffccff')} />
-            <Thread color={fromHex('#ab3400')} />
+            <Thread color={fromHex('#ffccff')} label="1" />
+            <Thread color={fromHex('#ab3400')} label="2" />
         </div>
     </Provider>
 );
