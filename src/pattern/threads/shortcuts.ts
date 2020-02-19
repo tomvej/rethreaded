c5adcdbfa893f2af1e1839d1868f48a7da4b2927
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-02-19 15:49:05 +0100

    selecting threads via keyboard

--- src/pattern/threads/shortcuts.ts
+++ src/pattern/threads/shortcuts.ts
@@ -1,8 +1,13 @@
-import {MOVE_LEFT, MOVE_RIGHT} from '~shortcuts';
+import {MOVE_LEFT, MOVE_RIGHT, SELECT_THREAD_1, SELECT_THREAD_2, SELECT_THREAD_3} from '~shortcuts';
 
-import {selectNextThread, selectPrevThread} from '../selection';
+import {selectNextThread, selectPrevThread, selectThread} from '../selection';
+
+const createSelectThread = (thread: number) => () => selectThread(thread);
 
 export default {
     [MOVE_LEFT]: selectPrevThread,
     [MOVE_RIGHT]: selectNextThread,
+    [SELECT_THREAD_1]: createSelectThread(0),
+    [SELECT_THREAD_2]: createSelectThread(1),
+    [SELECT_THREAD_3]: createSelectThread(2),
 };
