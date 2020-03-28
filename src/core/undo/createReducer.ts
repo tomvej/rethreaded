a1d04d2272144cd29d5de22d0087bbca788ecd2c
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-03-28 08:15:15 +0100

    initializing state

--- src/core/undo/createReducer.ts
+++ src/core/undo/createReducer.ts
@@ -7,21 +7,21 @@ import {REDO, UNDO} from './actions';
 
 
 export type UndoState<S> = {
-    current: S | undefined;
+    current: S;
     undo: Array<S>;
     redo: Array<S>;
 }
 
-const initialState = {
-    current: undefined,
+const wrapState = <S>(state: S): UndoState<S> => ({
+    current: state,
     undo: [],
-    redo: []
-};
+    redo: [],
+});
 
-const createReducer = <C, S, A extends Action>(reducer: ContextReducer<C, S, A>) => (state: UndoState<S> = initialState, action: A, context: C): UndoState<S> => {
+const createReducer = <C, S, A extends Action>(reducer: ContextReducer<C, S, A>, initialState: S) => (state = wrapState(initialState), action: A, context: C): UndoState<S> => {
     switch (action.type) {
         case UNDO: {
-            if (state.undo.length > 0 && state.current) {
+            if (state.undo.length > 0) {
                 return {
                     current: state.undo[state.undo.length - 1],
                     undo: state.undo.slice(0, state.undo.length - 1),
@@ -32,7 +32,7 @@ const createReducer = <C, S, A extends Action>(reducer: ContextReducer<C, S, A>)
             }
         }
         case REDO: {
-            if (state.redo.length > 0 && state.current) {
+            if (state.redo.length > 0) {
                 return {
                     current: state.redo[state.redo.length - 1],
                     undo: append(state.undo, state.current),
@@ -47,7 +47,7 @@ const createReducer = <C, S, A extends Action>(reducer: ContextReducer<C, S, A>)
             if (newState !== state.current) {
                 return {
                     current: newState,
-                    undo: state.current ? append(state.undo, state.current) : [],
+                    undo: append(state.undo, state.current),
                     redo: [],
                 };
             } else {
--- src/core/undo/selectors.ts
+++ src/core/undo/selectors.ts
@@ -1,5 +1,5 @@
 import {UndoState} from './createReducer';
 
-export const getCurrent = <S>(state: UndoState<S>): S | undefined => state.current;
+export const getCurrent = <S>(state: UndoState<S>): S => state.current;
 export const canUndo = <S>(state: UndoState<S>): boolean => state.undo.length > 0;
 export const canRedo = <S>(state: UndoState<S>): boolean => state.redo.length > 0;
