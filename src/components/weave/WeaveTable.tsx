037dcc6ea84400496e2800e46980a8cb46a08987
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-04-12 16:08:59 +0200

    zoom support

--- src/components/weave/WeaveTable.tsx
+++ src/components/weave/WeaveTable.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useCallback, useEffect, useRef, useState} from 'react';
 
 import {seq} from '~utils/array';
 
@@ -18,14 +18,35 @@ type WeaveTablePropTypes = {
 };
 
 const WeaveTable: FC<WeaveTablePropTypes> = ({tablets, rows, weaveComponent: WeaveComponent, repeat = 1}) => {
+    const [zoom, setZoom] = useState(1);
+    const onWheel = useCallback((event) => {
+        if (event.ctrlKey) {
+            event.preventDefault();
+            if (event.deltaY > 0) {
+                setZoom((zoom) => zoom * 1.2);
+            } else {
+                setZoom((zoom) => zoom / 1.2);
+            }
+        }
+    }, [setZoom]);
+    const ref = useRef<SVGSVGElement>(null);
+    useEffect(() => {
+        if (ref.current) {
+            const element = ref.current;
+            element.addEventListener('wheel', onWheel, {passive: false}); // chrome has passive wheel listeners on default
+            return () => element.removeEventListener('wheel', onWheel);
+        }
+    }, [ref.current, onWheel]);
+
     const width = tablets * THREAD_WIDTH;
     const height = (rows + 1) * WEAVE_LENGTH * repeat;
     return (
         <svg
             viewBox={`${-THREAD_WIDTH / 2}, ${-WEAVE_LENGTH}, ${width}, ${height}`}
-            width={width}
-            height={height}
+            width={width / zoom}
+            height={height / zoom}
             className={style.main}
+            ref={ref}
         >
             {seq(tablets).map((tablet) => (seq(rows * repeat).map((row) => (
                 <g
