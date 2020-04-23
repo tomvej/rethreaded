3d2f750d15af17c6d57b145d5239a18fc86ee00b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2020-04-23 14:17:13 +0200

    explanatory comment: record with nominal key typing
    deleteAt typing would assume the type was string literal and should be removed from the record

--- src/func/record.ts
+++ src/func/record.ts
@@ -1,3 +1,4 @@
 export * from 'fp-ts/es6/Record';
 
+// redeclared due to problems with nominally typed keys
 export declare function deleteAt<K extends string>(k: K): <A>(r: Record<K, A>) => Record<K, A>;
